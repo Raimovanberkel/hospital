c19b19bea948495e0417a897711b28f13dcee63f
MSI\Raimo van Berkel <Raimo.van.berkel@gmail.com>
2018-05-21 19:33:56 +0200

    species model en controller toegevoegd

---
+++ controller/SpeciesController.php
@@ -0,0 +1,43 @@
+<?php
+
+require(ROOT . "/model/SpeciesModel.php");
+
+// http://localhost/Hospital/client/index
+function index() {
+	return render('species/index');
+}
+
+function create() {
+	render("client/create");
+}
+
+function createSave() {
+	if (!createClient()) {
+		header('Location:' . URL . 'error/index');
+		exit();
+	}
+	header('Location:' . URL . 'client/index');
+}
+
+function delete($clientid) {
+	if (!deleteclient($clientid)) {
+		header("Location:" . URL . "error/index");
+		exit();
+	}
+	header("Location:" . URL . "client/index");
+}
+
+function edit($clientid) {
+	render("client/edit", array(
+		'client' => getclient($clientid)
+		));
+}
+
+function editSave($clientid) {
+	if (!editclient($clientid)) {
+		header('location:' . URL . 'error/index');
+	}
+	header('location:' . URL . 'client/index');
+} 
+
+?>
---
+++ model/SpeciesModel.php
@@ -0,0 +1,65 @@
+<?php
+
+function getclient($clientid) {
+	$db = openDatabaseConnection();
+	$sql = "SELECT * FROM clients WHERE client_id = :client_id";
+	$query = $db->prepare($sql);
+	$query->execute(array(":client_id" => $clientid));
+	$db = null;
+	return $query->fetch();
+}
+
+function createclient() {
+	$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+
+	
+	if (strlen($firstname) == 0 || strlen($lastname) == 0) {
+		return false;
+	}
+	
+	$db = openDatabaseConnection();
+	$sql = "INSERT INTO clients(client_firstname, client_lastname) VALUES (:firstname, :lastname)";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		':firstname' => $firstname,
+		':lastname' => $lastname));
+	$db = null;
+	return render('client/index');
+	return true;
+}
+
+function deleteclient($clientid = null) {
+		if (!$clientid) {
+		return false;
+	}
+	$db = openDatabaseConnection();
+	$sql = "DELETE FROM clients WHERE client_id = :client_id ";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		':client_id' => $clientid));
+	$db = null;
+	return render('client/index');
+	return true;
+}
+
+function editclient($clientid = null) {
+	$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+	$clientid = isset($_POST['clientid']) ? $_POST['clientid'] : null;
+
+	if (strlen($firstname) == 0 && strlen($lastname) == 0 ) {
+		return false;
+	}
+	
+	$db = openDatabaseConnection();
+
+	$sql = "UPDATE clients SET client_firstname = :client_firstname, client_lastname = :client_lastname WHERE client_id = :client_id";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		':client_firstname' => $firstname,
+		':client_lastname' => $lastname,
+		':client_id' => $clientid));
+	$db = null;
+	return true;
+}
