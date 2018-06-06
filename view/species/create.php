5f0b0c10182b5f075fc5224e9039224b2aa1008a
MSI\Raimo van Berkel <Raimo.van.berkel@gmail.com>
2018-06-06 09:00:13 +0200

    Done

--- view/species/create.php
+++ view/species/create.php
@@ -1,7 +1,7 @@
 
 <div class="header">
     <div class="card"><h1>New Species</h1></div>
-    <form action="<?= URL ?>species/createspecies" method="post">
+    <form action="<?= URL ?>species/createSave" method="post">
     <p>
         <label for="description">Description:</label>
         <input type="text" name="description" id="description">
